e870b9e5a5363701ed38449caebca1f3a68a5c11
Selim <selimdev00@gmail.com>
2024-01-12 11:29:40 +0500

    [TODO]: create & update DTOs

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTodoDto {}
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  @IsString({ message: 'Name must be a string' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Description is required' })
+  @IsString({ message: 'Description must be a string' })
+  description: string;
+
+  @IsNotEmpty({ message: 'IsCompleted is required' })
+  @IsBoolean({ message: 'IsCompleted must be a boolean value' })
+  isCompleted: boolean;
+}
--- src/todos/dto/update-todo.dto.ts
+++ src/todos/dto/update-todo.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+  @IsNotEmpty({ message: 'Name is required' })
+  @IsString({ message: 'Name must be a string' })
+  name?: string;
+
+  @IsOptional()
+  @IsString({ message: 'Description must be a string' })
+  description?: string;
+
+  @IsOptional()
+  @IsBoolean({ message: 'IsCompleted must be a boolean value' })
+  isCompleted?: boolean;
+}
